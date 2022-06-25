c6a162dc8dca8967e23640cec7514c1f0e39dca4
Aditya <aaak1837@gmail.com>
2022-06-26 00:43:19 +0530

    files added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing: {
+        100: "32rem",
+      },
+    },
+  },
+  plugins: [],
+};
